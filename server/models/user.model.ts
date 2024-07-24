31ba7d99b1f10ac3ac47b38f2acca549de6ff184
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-24 13:28:48 +0530

    modify

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -79,7 +79,7 @@ userSchema.pre<IUser>("save", async function (next) {
 // sign access token
 userSchema.methods.SignAccessToken = function () {
   return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
-    expiresIn: "5m",
+    expiresIn: "1d",
   });
 };
 
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -111,8 +111,8 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 export const accessTokenOptions: ITokenOptions = {
-  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),//5m accessTokenExpire=5
-  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  expires: new Date(Date.now() + accessTokenExpire*24 * 60 * 60 * 1000),//5m accessTokenExpire=5
+  maxAge: accessTokenExpire*24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "lax"
 }
