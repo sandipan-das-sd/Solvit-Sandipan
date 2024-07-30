8b1a7c861ec181199357c926a3fadc90aa404f0e
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-30 08:01:39 +0530

    updated

--- server/build/app.js
+++ server/build/app.js
@@ -29,7 +29,7 @@ exports.app.use((0, express_fileupload_1.default)({
     tempFileDir: '/tmp/'
 }));
 exports.app.use((0, cors_1.default)({
-    origin: ["https://solvit-client.vercel.app"],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
