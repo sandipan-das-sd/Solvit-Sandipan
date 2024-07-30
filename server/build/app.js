13040e04fb12f22cdd7637416bd6345bf9fcf4e9
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-31 02:10:36 +0530

    changes

--- server/build/app.js
+++ server/build/app.js
@@ -29,7 +29,7 @@ exports.app.use((0, express_fileupload_1.default)({
     tempFileDir: '/tmp/'
 }));
 exports.app.use((0, cors_1.default)({
-    origin: ["https://www.solvit.live"],
+    origin: ["https://solvit-test-deploy.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
