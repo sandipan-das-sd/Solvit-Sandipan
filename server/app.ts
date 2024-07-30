389a69d2250fdf99c43e99d265996fee576a1579
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-30 07:57:40 +0530

    cors updated

--- server/app.ts
+++ server/app.ts
@@ -27,7 +27,7 @@ app.use(fileUpload({
 }));
 app.use(
     cors({
-        origin: ["https://solvit-client.vercel.app"],
+        origin: "*",
         methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
