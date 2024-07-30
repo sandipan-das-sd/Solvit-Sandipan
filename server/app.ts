5021c0131c68be433f40b94c322bbd32d901b28b
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-30 19:03:21 +0530

    test

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
+
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
